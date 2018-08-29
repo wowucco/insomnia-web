e364dedefafb5a7a7ba812d6b52a678dd36a3e7e
Vladimir Scherbak <vscherbak@newagesol.com>
2018-08-29 17:22:22 +0300

    init

---
+++ client/routes.js
@@ -0,0 +1,13 @@
+/**
+ * Created by wowucco on 29.08.18.
+ */
+import React from 'react';
+import {Router, Route, browserHistory} from 'react-router';
+
+import Home from './views/home';
+
+export default () => (
+  <Router history={browserHistory}>
+    <Route path='/' component={Home}/>
+  </Router>
+);
