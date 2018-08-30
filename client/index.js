dd4ca3ec1bdbf922ac83919be619a49699067e29
Vladimir Scherbak <vscherbak@newagesol.com>
2018-08-30 11:56:09 +0300

    react-app

--- client/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by wowucco on 29.08.18.
- */
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import AppRouter from './routes';
-
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
